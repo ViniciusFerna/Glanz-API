9e60d9ba26035ca6c25534a308047b347d3f9166
ViniciusFerna <vinicius020206@gmail.com>
2025-05-30 19:27:01 -0300

    Atualização CORS v2

--- src/main/java/com/eventos/glanz/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eventos.glanz.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.eventos.glanz.model.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-
-}
