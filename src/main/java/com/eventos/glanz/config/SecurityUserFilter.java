5021e112e85dbb426cbfbb6e2890d73d656d50ef
Vinícius Fernandes <vinicius020206@gmail.com>
2025-06-06 21:32:06 -0300

    update

--- src/main/java/com/eventos/glanz/config/SecurityUserFilter.java
+++ src/main/java/com/eventos/glanz/config/SecurityUserFilter.java
@@ -19,49 +19,51 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class SecurityUserFilter extends OncePerRequestFilter{
+public class SecurityUserFilter extends OncePerRequestFilter {
 
 	@Autowired
 	private JWTProvider jwtProvider;
-	
-		
-		private String getTokenFromHeader(HttpServletRequest request) {
-			String header = request.getHeader("Authorization");
-			
-			if (header == null || !header.startsWith("Bearer ")) {
-				return null;
-			}
-			
-			return header.replace("Bearer ", "").trim();		
-			}
-		
-		@Override
-		protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-				FilterChain filterChain) throws IOException, ServletException {
-			String token = getTokenFromHeader(request);
-			
-			if(token == null) {
-				filterChain.doFilter(request, response);
-				return;
-			}
-			
-			DecodedJWT decodedJWT = jwtProvider.validateToken(token);
-			if(decodedJWT == null) {
-				response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token Inválido");
-				return;
-			}
-			
-			String role = decodedJWT.getClaim("roles").asString();
-			
-			UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
-					decodedJWT.getSubject(),
-					null,
-					Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role))
-				);
-			
-			SecurityContextHolder.getContext().setAuthentication(auth);
+
+	private String getTokenFromHeader(HttpServletRequest request) {
+		String header = request.getHeader("Authorization");
+
+		if (header == null || !header.startsWith("Bearer ")) {
+			return null;
+		}
+
+		System.out.println("Passou no corte do Bearer");
+
+		return header.replace("Bearer ", "").trim();
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws IOException, ServletException {
+		String token = getTokenFromHeader(request);
+
+		if (token == null) {
 			filterChain.doFilter(request, response);
-			
-		}	
-	
+			return;
+		}
+
+		System.out.println("Token passou do null");
+		
+		DecodedJWT decodedJWT = jwtProvider.validateToken(token);
+		if (decodedJWT == null) {
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token Inválido");
+			return;
+		}
+		
+		System.out.println("Passou no decode Token");
+		
+		String role = decodedJWT.getClaim("roles").asString();
+
+		UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(decodedJWT.getSubject(),
+				null, Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role)));
+
+		SecurityContextHolder.getContext().setAuthentication(auth);
+		filterChain.doFilter(request, response);
+
+	}
+
 }
